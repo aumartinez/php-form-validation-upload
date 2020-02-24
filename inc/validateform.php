924c3f2092848efce2666cf61b6223cbc99e6a07
Augusto Martinez <acm_asd@hotmail.com>
2020-02-23 18:13:36 -0600

    Update validateform.php

--- inc/validateform.php
+++ inc/validateform.php
@@ -216,7 +216,7 @@ class Validateform extends Model {
   public function upload() {
     $target_dir = "../uploads/";
     $target_file = $target_dir . basename($_FILES["images"]["name"]);
-    $uploadOk = 1;
+    $upload_ok = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     
     # Check if image file is a actual image or fake image
@@ -224,34 +224,37 @@ class Validateform extends Model {
         $check = getimagesize($_FILES["images"]["tmp_name"]);
         if($check !== false) {
             echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
+            $upload_ok = 1;
         } else {
             echo "File is not an image.";
-            $uploadOk = 0;
+            $upload_ok = 0;
         }
     }
     
     # Check if file already exists
     if (file_exists($target_file)) {
         echo "Sorry, file already exists.";
-        $uploadOk = 0;
+        $upload_ok = 0;
     }
     
     # Check file size
     if ($_FILES["images"]["size"] > 500000) {
         echo "Sorry, your file is too large.";
-        $uploadOk = 0;
+        $upload_ok = 0;
     }
-    // Allow certain file formats
+    
+    # Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
         echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
+        $upload_ok = 0;
     }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
+    
+    # Check if $uploadOk is set to 0 by an error
+    if ($upload_ok == 0) {
         echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
+    
+    # if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["images"]["tmp_name"], $target_file)) {
             echo "The file ". basename( $_FILES["images"]["name"]). " has been uploaded.";
